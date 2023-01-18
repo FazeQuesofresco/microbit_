1e79912a0bbf0e03367f2b308bed98071f7ad8cb
FazeQuesofresco <114920060+FazeQuesofresco@users.noreply.github.com>
2023-01-18 13:46:15 +0100

    Create tarea4.py

---
+++ tarea4.py
@@ -0,0 +1,16 @@
+from microbit import *
+forma1 = Image("99909:00909:99999:90900:90999")
+forma2 = Image("33303:00303:33333:30300:30333")
+forma3 = Image("99909:00909:99699:90900:90999")
+forma4 = Image("44404:00404:44444:40400:40444")
+
+
+while True:
+    display.show(forma1)
+    sleep(200)
+    display.show(forma2)
+    sleep(200)
+    display.show(forma3)
+    sleep(200)
+    display.show(forma4)
+    sleep(200)

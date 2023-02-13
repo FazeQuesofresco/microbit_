0977e9b6a2ae1d627c7ea8870edfd8f43caf5e5e
FazeQuesofresco <114920060+FazeQuesofresco@users.noreply.github.com>
2023-02-13 13:20:02 +0100

    Update cancion.py

--- cancion.py
+++ cancion.py
@@ -5,5 +5,6 @@ import music
 
 ltsNotas =['f4:2','g4:2','e4:2','c4:2','D4:3','D4:2','a4:2','g4:3','f4:3','e4:3','e4:2','e4:2','g4:3','f4:2','e4:2','D4:3','D4:2','F4:2','e4:2','F4:2','e4:2','F4:2','D4:3','D4:2']
 
-music.play(ltsNotas)
-
+while True:
+    music.play(ltsNotas)
+    sleep(200)

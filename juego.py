b1d21ba627dca3e28374f76725bf329be4bdc97b
FazeQuesofresco <114920060+FazeQuesofresco@users.noreply.github.com>
2023-02-15 13:39:39 +0100

    Create juego.py

---
+++ juego.py
@@ -0,0 +1,27 @@
+#imports go at the top
+from microbit import*
+import random
+
+vocales =['A','E','I','O','U']
+consonantes=['B','C','D','F','G','H','J','K','L','M','N','Ñ','P','Q','R','S','T','V','W','X','Y','Z',]
+total_palabras = 9
+while total_palabras>0:
+    if button_a.was_pressed():
+        vocal_aleatoria=vocales[random.randint(0,len(vocales)-1)]
+        display.show(vocal_aleatoria)
+        total_palabras=total_palabras-1
+        sleep(1000)
+        
+    elif button_b.was_pressed():
+        display.show("?")
+    if button_b.was_pressed():
+        consonante_aleatoria=consonantes[random.randint(0,len(consonantes)-1)]
+        display.show(consonante_aleatoria)
+        total_palabras=total_palabras-1
+        sleep(1000)
+    else:
+        display.show("?")
+   
+
+
+display.scroll("Fin")

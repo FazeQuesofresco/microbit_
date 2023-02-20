ce3b8add6905641e51a7315a76fa5c3d653304b2
FazeQuesofresco <114920060+FazeQuesofresco@users.noreply.github.com>
2023-02-20 13:00:15 +0100

    Create tarea3.py

---
+++ tarea3.py
@@ -0,0 +1,16 @@
+
+# Imports go at the top
+from microbit import *
+contador = 0
+
+while True:
+    if button_a.get_presses():
+        contador = contador + 1
+    elif button_b.get_presses():
+        contador = contador - 1
+    elif button_a.is_pressed() and button_b.is_pressed():
+        contador = 0
+    elif pin_logo.is_touched():
+        display.show("Pepe")
+    else:
+        display.show(contador)

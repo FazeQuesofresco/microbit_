a3f474214bac329ebdd6f3bfcae760b1580bad03
FazeQuesofresco <114920060+FazeQuesofresco@users.noreply.github.com>
2023-02-20 12:58:36 +0100

    Create tarea1.py

---
+++ tarea1.py
@@ -0,0 +1,9 @@
+
+# Imports go at the top
+from microbit import *
+
+# Variable nombre
+nombre = "José"
+apellido = "Jorge"
+
+display.scroll(nombre +" "+ apellido, wait=False, loop= True, delay= 80)
